19beffbadb0f8ec1a24c2c94f0f44b3ae7c965bc
renan <renanrosetti@gmail.com>
2021-06-21 16:12:25 -0300

    Initial commit

---
+++ src/listas_de_exercicios/Main.java
@@ -0,0 +1,8 @@
+package listas_de_exercicios;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/listas_de_exercicios/caso1_estru_seque.java
@@ -0,0 +1,6 @@
+package listas_de_exercicios;
+
+public class caso1_estru_seque {
+
+
+}
